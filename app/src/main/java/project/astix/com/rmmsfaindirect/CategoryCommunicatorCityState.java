9572bfb6ac5575e103f8b5388ebe086234ff02f0
Shivam <astixset2@gmail.com>
2018-07-27 15:39:14 +0530

    initial commit

---
+++ app/src/main/java/project/astix/com/rmmsfaindirect/CategoryCommunicatorCityState.java
@@ -0,0 +1,10 @@
+package project.astix.com.rmmsfaindirect;
+
+import android.app.Dialog;
+
+public interface CategoryCommunicatorCityState {
+	
+	public void selectedCityState(String selectedCategory, Dialog dialog, int flgCityState);
+
+
+}
